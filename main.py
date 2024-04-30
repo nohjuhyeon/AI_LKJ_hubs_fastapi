efa6036b7bf6ca92d45619bd8a27b38da2c5f197
nohjuhyeon <njh2720@gmail.com>
2024-04-30 03:13:36 +0000

    ㅎ

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ from routes.admin import router as admin_router
 from routes.mypage import router as second_router
 from routes.plan_trip import router as users_router
 from routes.consult import router as consult_router
+from routes.consult_api import router as consult_api_router
 from routes.event import router as event_router
 from routes.detailed_region import router as detailed_router
 
@@ -41,6 +42,7 @@ app.include_router(admin_router, prefix="/admin")
 app.include_router(second_router, prefix="/mypage")
 app.include_router(users_router, prefix="/plan_trip")
 app.include_router(consult_router, prefix="/consult")
+app.include_router(consult_api_router, prefix="/consult_api")
 app.include_router(event_router, prefix="/event")
 app.include_router(detailed_router, prefix="/detailed_region")
 
--- routes/consult_api.py
+++ routes/consult_api.py
@@ -3,20 +3,33 @@ from typing import List
 from beanie import PydanticObjectId
 from databases.connections import Database
 from fastapi import APIRouter, Depends, HTTPException, status
-from models.admin_notice import Notice
-from models.frequent_CS import Frequent_CS
-from models.data_chart import Data_chart
+from models.admin_notice import Admin_notice_list
+from models.frequent_CS import FAQ_list
+from models.data_chart import data_attraction, data_concept_search, data_consume,data_consume_transition,data_trend_search
 
 router = APIRouter(
     tags=["Consults"]
 )
 
-notice_database = Database(Notice)
-frequent_CS_database = Database(Frequent_CS)
-data_chart_database = Database(Data_chart)
-
+notice_database = Database(Admin_notice_list)
+frequent_CS_database = Database(FAQ_list)
+collection_data_attraction=Database(data_attraction)
+collection_data_concept_search=Database(data_concept_search)
+collection_data_consume=Database(data_consume)
+collection_data_consume_transition=Database(data_consume_transition)
+collection_data_trend_search=Database(data_trend_search)
 #공지사항
 @router.get("/user_notice")
 async def user_notice() :
-    notice = await notice_database.get(id)
+    notice = await notice_database.get_all()
     return notice
+
+@router.get("/FAQ_list")
+async def user_notice() :
+    notice = await frequent_CS_database.get_all()
+    return notice
+
+# @router.get("/Data_chart")
+# async def user_notice() :
+#     notice = await notice_database.get_all()
+#     return notice
