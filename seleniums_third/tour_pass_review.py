99d0cb4301c993bee165d78aefe6153a8ef0bd13
nohjuhyeon <njh2720@gmail.com>
2024-05-10 16:30:56 +0900

    ww

--- seleniums_third/tour_pass_review.py
+++ seleniums_third/tour_pass_review.py
@@ -29,9 +29,8 @@ browser = webdriver.Chrome(service = ChromeService(webdriver_manager_directory),
 capabilities = browser.capabilities
 
 pass
-browser.get("https://search.shopping.naver.com/search/all?adQuery=%ED%88%AC%EC%96%B4%ED%8C%A8%EC%8A%A4&npayType=&origQuery=%ED%88%AC%EC%96%B4%ED%8C%A8%EC%8A%A4&pagingIndex=1&pagingSize=40&productSet=checkout&query=%ED%88%AC%EC%96%B4%ED%8C%A8%EC%8A%A4&sort=review&timestamp=&viewType=list")                                     # - 주소 입력
+browser.get("https://smartstore.naver.com/lscompany01/category/e4a8e9ad47c74cbebb8457c9995f2f38?cp=1")                                     # - 주소 입력
 
-                                                    # - 가능 여부에 대한 OK 받음
 pass
 html = browser.page_source                          # - html 파일 받음(and 확인)
 # print(html)
@@ -41,31 +40,74 @@ from selenium.webdriver.common.keys import Keys
 # browser.save_screenshot('./formats.png')     
 
 # 여러개 동영상 collection 있을때 버튼
-tour_pass_list = browser.find_elements(by=By.CSS_SELECTOR,value='div.product_title__Mmw2K > a')
+tour_pass_list = browser.find_elements(by=By.CSS_SELECTOR,value='#CategoryProducts > ul > li > div > a > strong')
 
 time.sleep(2)
 for i in range(len(tour_pass_list)):
-    tour_pass_list = browser.find_elements(by=By.CSS_SELECTOR,value='div.product_title__Mmw2K > a')
+    time.sleep(1)
+    tour_pass_list = browser.find_elements(by=By.CSS_SELECTOR,value='#CategoryProducts > ul > li > div > a > strong')
     if '투어 패스' in tour_pass_list[i].text or '투어패스' in tour_pass_list[i].text:
+        title = tour_pass_list[i].text
         if '부산' in tour_pass_list[i].text:
             region = '부산'
+            tour_pass_list[i].click()
         elif '경기' in tour_pass_list[i].text:
             region = '경기'
+            tour_pass_list[i].click()
         elif '강릉' in tour_pass_list[i].text:
             region = '강원'
+            tour_pass_list[i].click()
         elif '강원' in tour_pass_list[i].text:
             region = '강원'
+            tour_pass_list[i].click()
         elif '제주' in tour_pass_list[i].text:
             region = '제주'
-
-        tour_pass_list[i].click()
-        
-        time.sleep(1)
+            tour_pass_list[i].click()
+        elif '여수' in tour_pass_list[i].text:
+            region = '전남'
+            tour_pass_list[i].click()
+        elif '충남' in tour_pass_list[i].text:
+            region = '충남'
+            tour_pass_list[i].click()
+        elif '전북' in tour_pass_list[i].text:
+            region = '전북'
+            tour_pass_list[i].click()
+                    
+        time.sleep(2)
         # 설명 더보기 버튼
     
-    print(tour_pass_list[i].text)
+    print(title)
     print(region)
-    browser.back()
+    review_box = browser.find_element(by=By.CSS_SELECTOR,value='#content > div > div.z7cS6-TO7X > div.FtuIjPoTdk > div._2jrC4tvN2w > div > a')
+    review_box.click()
+    time.sleep(1)
+    while True:
+        try:
+            review_list = browser.find_elements(by=By.CSS_SELECTOR,value='#REVIEW > div > div._2LvIMaBiIO > div._2g7PKvqCKe > ul > li > div > div._3P12lHpgMB > div')
+            for review_element in review_list:
+                rating = review_element.find_element(by=By.CSS_SELECTOR,value='div._2V6vMO_iLm > em').text
+                date = review_element.find_element(by=By.CSS_SELECTOR,value='div.iWGqB6S4Lq > span').text
+                content = review_element.find_element(by=By.CSS_SELECTOR,value='div._3z6gI4oI6l > div > span').text
+                print(title)
+                print(region)
+                print(rating)
+                print(date)
+                print(content)
+                collection.insert_one({'title':title,
+                            'region':region,
+                            'good': rating,
+                            'date':date,
+                            'content':content})
+            next_btn = browser.find_element(by=By.CSS_SELECTOR, value='a._2Ar8-aEUTq')
+            next_btn.click()
+            time.sleep(1)
+        except:
+            break    
     pass
+    element_body = browser.find_element(by=By.CSS_SELECTOR,value="body")
 
-browser.quit()                                      # - 브라우저 종료
+    element_body.send_keys(Keys.HOME)
+    time.sleep(1)
+    back_btn = browser.find_element(by=By.CSS_SELECTOR,value='#content > div > div._3U1EEdeAc6 > div._2u_hEOSeOu > div > ul > li:nth-child(2) > a')
+    back_btn.click()                                      # - 브라우저 종료
+    
